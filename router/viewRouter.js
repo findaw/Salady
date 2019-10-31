95f13038d9f4b2135dabeb3255f02beb8c039000
findaw <wqre1@naver.com>
2019-10-31 19:27:58 +0900

    회원가입 구분 페이지 작성 #22

--- router/viewRouter.js
+++ router/viewRouter.js
@@ -8,8 +8,12 @@ router.use(express.static("client/public"));
 router.get("/login", (req, res)=>{
     res.status(200).render("login");
 });
+
 router.get("/join", (req, res)=>{
     res.status(200).render("join");
+});
+router.get("/join/member", (req, res)=>{
+    res.status(200).render("joinMember");
 });
 router.get("/welcome", (req, res)=>{
     res.status(200).render("welcome");
